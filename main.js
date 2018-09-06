8922e5dc0ad4cb6015eccc121ced136211361b0b
Antoine-marchais <antoine.marchais@student.ecp.fr>
2018-09-06 23:45:18 +0200

    "ix port assignment

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 //const token = "514706312:AAGU4Gf5Gl1rmPCwK-LWE7CFPrhW7eP2xNM";
 
-var express = require("express");
-
-var app = express();
+const express = require("express");
+const PORT = process.env.PORT;
+const app = express();
 
 app.get("/",function(req,res){
     res.setHeader('Content-Type','text/plain');
     res.send('ceci est la réponse')
 });
 
-app.listen(8080);
+app.listen(PORT);
