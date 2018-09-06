f1cdbd7decada05d0c1e4d3f8e3aefbe9c1a0430
Antoine-marchais <antoine.marchais@student.ecp.fr>
2018-09-06 22:11:26 +0200

    fixed response

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-const token = "514706312:AAGU4Gf5Gl1rmPCwK-LWE7CFPrhW7eP2xNM";
+//const token = "514706312:AAGU4Gf5Gl1rmPCwK-LWE7CFPrhW7eP2xNM";
+
+var express = require("express");
+
+var app = express();
+
+app.get("/",function(req,res){
+    res.setHeader('Content-Type','text/plain');
+    res.send('ceci est la réponse')
+});
+
+app.listen(8080);
