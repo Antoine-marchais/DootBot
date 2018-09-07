3a3f3bc6174edcb3c1b1b3483bb9cb0539c249cd
Antoine-marchais <antoine.marchais@student.ecp.fr>
2018-09-07 18:32:45 +0200

    generate update template

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const token = "514706312:AAEDQqSaQ-s8YDB7VvepQGjQDMCJNoUFf1Q";
 const express = require("express");
 const request = require("request");
 const bodyParser = require('body-parser');
+const fs = require('fs');
 const PORT = process.env.PORT;
 const app = express();
 let count = 0;
@@ -20,8 +21,9 @@ app.get("/",function(req,res){
 });
 
 app.post("/",function(req,res){
-    res.setHeader('Content-Type','text/plain');
+    res.status(200);
     count += 1;
+    fs.writeFile("./update.json",req.body,"utf-8",function(){console.log("file was written");});
     const id = req.body.result[0].message.chat.id;
     const doot = 'd'+count*'o'+'t';
     const message = Message(id,doot); 
