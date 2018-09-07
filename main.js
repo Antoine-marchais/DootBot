c685946e87159c4561c89914ff25c003f62d5950
Antoine-marchais <antoine.marchais@student.ecp.fr>
2018-09-07 02:16:42 +0200

    add response

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-//const token = "514706312:AAGU4Gf5Gl1rmPCwK-LWE7CFPrhW7eP2xNM";
+const token = "514706312:AAEDQqSaQ-s8YDB7VvepQGjQDMCJNoUFf1Q";
 
 const express = require("express");
+const request = require("request");
 const PORT = process.env.PORT;
 const app = express();
 let count = 0;
+
+function Message(chat_id,text){
+    this.chat_id = chat_id;
+    this.text = text;
+}
 
 app.get("/",function(req,res){
     res.setHeader('Content-Type','text/plain');
@@ -14,6 +20,7 @@ app.get("/",function(req,res){
 app.post("/",function(req,res){
     res.setHeader('Content-Type','text/plain');
     count += 1;
+    request('https://api.telegram.org/bot'+token+'/sendMessage',)
     res.status(200);
     res.send('ok');
 });
