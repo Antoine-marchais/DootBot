a358fe34e2160560f63552a7c0b91e024e754b49
Antoine-marchais <antoine.marchais@student.ecp.fr>
2018-09-10 17:46:28 +0200

    fix module

--- TelegramBot.js
+++ TelegramBot.js
@@ -3,6 +3,11 @@ const request = require("request");
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT;
 
+function Message(chat_id,text){
+    this.chat_id = chat_id;
+    this.text = text;
+};
+
 function Bot(token,route){
     this.url = url;
     this.token = token;
@@ -16,22 +21,17 @@ function Bot(token,route){
             res.status(200);
             callback(message);
         });
+    };
+
+    this.sendMessage = function(id,text){
+        const message = new Message(id,text); 
+        const options = {
+            url: 'https://api.telegram.org/bot'+this.token+'/sendMessage',
+            json: true,
+            body: text
+        };
+        request.post(options,function(err,res,body){
+            console.log("message send");
+        });
     }
 }
-
-function Message(chat_id,text){
-    this.chat_id = chat_id;
-    this.text = text;
-};
-
-const sendMessage = function(token,id,text){
-    const message = new Message(id,text); 
-    const options = {
-        url: 'https://api.telegram.org/bot'+token+'/sendMessage',
-        json: true,
-        body: text
-    };
-    request.post(options,function(err,res,body){
-        console.log("message send");
-    });
-};
